3034208f34ff830c55f1c4ecc231e9f73e5c81db
Saloni Ajmera <saloniajmera@Salonis-MacBook-Pro.local>
2018-05-12 12:56:12 -0700

    handled corner cases

--- Array/reverse_of_array.py
+++ Array/reverse_of_array.py
@@ -6,17 +6,28 @@ def reverse_array(array):
 	:param start: starting pointer
 	:param end: ending pointer
 	"""
-    start = 0
-    end = len(array)-1
+	array_length = len(array)
+	if array_length < 2 : # if array length is 0 or 1, return array
+		return array
+    
+	start = 0
+	end = array_length - 1
 	for char in array:
 		temp = array[start]  # store the start element in the temp  variable 
 		array[start] = array[end]   # end element replaced to start 
 		array[end] = temp  # temp element to end position 
-		start = start + 1
-		end = end -1 
+		start =+1
+		end =-1 
 		if start > end: 
 			return array
 
 
-array = ['a', 'b', 'c', 'd']
+array = [1,2,3]
 print reverse_array(array)
+
+
+"""
+    corner casese:
+    1.what if array is empty?
+    2.what if array has only one element
+"""
